8a1457049823ee0167542c650c89d3224cb1fcc8
Mehedi Hasan <mehedihasanhrid1@gmail.com>
2024-01-23 16:29:46 +0600

    drop down button implimented

--- frontend/script.js
+++ frontend/script.js
@@ -1,72 +1,65 @@
 //fetched data
 
 async function cryptoData() {
-    try {
-      const result = await fetch("http://localhost:5000/crypto");
-        const data = await result.json();
-        return data;
-    } catch (error) {
-      console.error("Error:", error);
-      return [];
-    }
+  try {
+    const result = await fetch("http://localhost:5000/crypto");
+    const data = await result.json();
+    return data;
+  } catch (error) {
+    console.error("Error:", error);
+    return [];
   }
-
-
+}
 
 // //dark mode funtionality
 
-document.addEventListener('DOMContentLoaded', function () {
-    const darkModeCheckbox = document.getElementById('darkModeCheckbox');
-    const body = document.body;
+document.addEventListener("DOMContentLoaded", function () {
+  const darkModeCheckbox = document.getElementById("darkModeCheckbox");
+  const body = document.body;
 
-    function toggleDarkMode() {
-        body.classList.toggle('dark-theme', darkModeCheckbox.checked);
-        const darkModeStatus = darkModeCheckbox.checked ? 'enabled' : 'disabled';
-        localStorage.setItem('darkMode', darkModeStatus);
-    }
+  function toggleDarkMode() {
+    body.classList.toggle("dark-theme", darkModeCheckbox.checked);
+    const darkModeStatus = darkModeCheckbox.checked ? "enabled" : "disabled";
+    localStorage.setItem("darkMode", darkModeStatus);
+  }
 
-    darkModeCheckbox.addEventListener('change', toggleDarkMode);
+  darkModeCheckbox.addEventListener("change", toggleDarkMode);
 
-    const darkModeSetting = localStorage.getItem('darkMode');
-    if (darkModeSetting === 'enabled') {
-        darkModeCheckbox.checked = true;
-        toggleDarkMode();
-    }
+  const darkModeSetting = localStorage.getItem("darkMode");
+  if (darkModeSetting === "enabled") {
+    darkModeCheckbox.checked = true;
+    toggleDarkMode();
+  }
 });
 
-document.addEventListener('DOMContentLoaded', async function () {
-    const timerCounter = document.getElementById('timer-counter');
+document.addEventListener("DOMContentLoaded", async function () {
+  const crypto = await cryptoData();
+  const timerCounter = document.getElementById("timer-counter");
 
-        function startTimer() {
-        let count = 60;
+  function startTimer() {
+    let count = 60;
 
-        function updateCounter() {
-            timerCounter.textContent = count;
-            count--;
+    function updateCounter() {
+      timerCounter.textContent = count;
+      count--;
 
-            if (count < 1) {
-                count = 60;
-            }
-        }
-
-        updateCounter();
-        setInterval(updateCounter, 1000);
+      if (count < 1) {
+        count = 60;
+      }
     }
 
-    startTimer();
+    updateCounter();
+    setInterval(updateCounter, 1000);
+  }
 
-    const data = await cryptoData();
-    const button = document.getElementById('dropdown-button');
-  data.forEach(item => {
+  startTimer();
+
+  const button = document.getElementById("dropdown-button");
+  crypto.forEach((item) => {
     const option = document.createElement("option");
     option.value = item.base_unit;
     option.text = item.base_unit;
+    option.className = "option-dropdown";
     button.appendChild(option);
   });
-
 });
-
-
-async function showDropdown() {
-    
-  }
